44c5eed39b054b7ee5b2f8677f4c42db4f15d8a7
Diógenes Augusto Fernandes Hermínio <diofeher@gmail.com>
2018-12-21 11:12:35 -0300

    Improving the logic to test max players

--- lib/game_interaction.rb
+++ lib/game_interaction.rb
@@ -7,7 +7,7 @@ class GameInteraction
   def initialize
     number = 0
     puts "Select the number of players (MAX: #{MAX_PLAYERS}): "
-    number = gets.to_i while number.zero? || (number > MAX_PLAYERS || number < 0)
+    number = gets.to_i while number <= 0 || number > MAX_PLAYERS
     @game = Game.new(number)
   end
 
